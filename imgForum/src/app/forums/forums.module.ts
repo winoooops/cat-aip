f9b0cf952aded359239fd34fcc270c970eb0b344
Wei Wang <13118157@student.uts.edu.au>
2019-08-14 19:29:11 +1000

    barebones for forums route

---
+++ imgForum/src/app/forums/forums.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ForumsComponent } from './forums.component';
+
+
+
+@NgModule({
+  declarations: [ForumsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ForumsModule { }
