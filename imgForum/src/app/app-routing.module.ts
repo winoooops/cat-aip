080521c38fabea92c1c5bdcd2e4b9a4ba93ff3b1
Wei Wang <13118157@student.uts.edu.au>
2019-08-14 19:29:38 +1000

    define minimal routing with lazy loading the forums route...

--- imgForum/src/app/app-routing.module.ts
+++ imgForum/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'forums', loadChildren: 'app/forums/forums.module#ForumsModule'}, 
+  { path: '', redirectTo: '/forums', pathMatch: 'full'} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
