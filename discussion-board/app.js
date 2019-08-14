94fa7be5624fb5bec6651a0262b6d884c9ec4235
weiwang <winoooops@outlook.com>
2019-08-14 10:35:32 +1000

    created the first express route: home

--- discussion-board/app.js
+++ discussion-board/app.js
@@ -2,16 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const app = express()
-
 const PORT = 3000
+
+const homeRoute = require('./routes/home')
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello</h1>')
-})
+app.use('/', homeRoute)
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`)
