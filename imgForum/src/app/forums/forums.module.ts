6015d264e8f5aeee6326e305613c3c177908ba62
Wei Wang <13118157@student.uts.edu.au>
2019-08-14 19:34:07 +1000

    create forums sub router in forums.module

--- imgForum/src/app/forums/forums.module.ts
+++ imgForum/src/app/forums/forums.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForumsComponent } from './forums.component';
+import { Routes, RouterModule } from '@angular/router'
+import { ForumComponent } from '../forum/forum.component';
 
+const routes: Routes = [
+  { path: '', component: ForumsComponent }, 
+  { path: ':forum_id', component: ForumComponent }
+]
 
 
 @NgModule({
   declarations: [ForumsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild( routes )
   ]
 })
 export class ForumsModule { }
