081fb07423bce38f87deec4e36e1862ccdaa081c
weiwang <winoooops@outlook.com>
2019-08-14 12:24:31 +1000

    add the multer configuration learned from multer's doc and youtube channel Traversy Media...

--- discussion-board/server/routes/forums.js
+++ discussion-board/server/routes/forums.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
+const multer = require("multer")
+
+// set up multer diskStorage
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    // the image will be saved as img-upload-<date>.extension 
+  }
+})
+const upload = multer({ storage }).single('img-upload')
+
 
 router.get('/', (req, res) => {
   res.render('forums', { title: "forums" })
