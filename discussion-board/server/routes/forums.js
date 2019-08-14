767a9536175f08bbb9817b3f80b7a16ceeec7451
weiwang <winoooops@outlook.com>
2019-08-14 11:57:53 +1000

    rename the routes and views to map the actual routes that can be used in ass3

--- discussion-board/server/routes/forums.js
+++ discussion-board/server/routes/forums.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('home', { title: "home" })
+  res.render('home', { title: "forums" })
 })
 
 module.exports = router
