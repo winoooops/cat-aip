fedb18741837a2db98290f62246ed4819ec1e84f
weiwang <winoooops@outlook.com>
2019-08-14 10:39:43 +1000

    template engine setup

--- discussion-board/app.js
+++ discussion-board/app.js
@@ -6,10 +6,15 @@ const PORT = 3000
 
 const homeRoute = require('./routes/home')
 
-
+// configurative middlewars
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
+
+
+// to use handlebars as template
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use('/', homeRoute)
 
