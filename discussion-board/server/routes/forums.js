604d3d1c29b2d15aee86599ed067d4cc0c94f863
weiwang <winoooops@outlook.com>
2019-08-14 12:01:00 +1000

    add upload route...

--- discussion-board/server/routes/forums.js
+++ discussion-board/server/routes/forums.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('home', { title: "forums" })
+  res.render('forums', { title: "forums" })
 })
+
+router.get('/upload', (req, res) => {
+  res.render('upload', { title: "upload" })
+})
+
 
 module.exports = router
