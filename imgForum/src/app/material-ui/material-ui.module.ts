cd63786ae18c36014f1249de76946ccf5ee1a1a7
Wei Wang <13118157@student.uts.edu.au>
2019-08-14 21:54:44 +1000

    forgot to export the maticon...add it

--- imgForum/src/app/material-ui/material-ui.module.ts
+++ imgForum/src/app/material-ui/material-ui.module.ts
@@ -24,6 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     MatButtonModule,
     MatInputModule,
     MatMenuModule,
+    MatIconModule,
     BrowserAnimationsModule
   ]
 })
