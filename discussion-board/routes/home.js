aa2e6b9db1efd9f79bae336809335bc27f0d2dff
weiwang <winoooops@outlook.com>
2019-08-14 10:39:59 +1000

    let home route render home template...

--- discussion-board/routes/home.js
+++ discussion-board/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('World')
+  res.render('home', { title: "home" })
 })
 
 module.exports = router
