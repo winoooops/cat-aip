06f5254d7ded1ac3f70d3a9c9abd920c11636180
weiwang <winoooops@outlook.com>
2019-08-19 12:07:53 +1000

    consume the service in the post-form component, supposely...

--- imgForum/src/app/forum-post/forum-post.component.ts
+++ imgForum/src/app/forum-post/forum-post.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-forum-post',
   templateUrl: './forum-post.component.html',
   styleUrls: ['./forum-post.component.scss']
 })
-export class ForumPostComponent implements OnInit {
+export class ForumPostComponent {
+  userID: string = '001'
+  uploadResponse = { status: '', message: '', filePath: '' };
+  error: string
+  constructor(private postService: PostService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit(e) {
+    const formData = new FormData()
+    this.postService.upload(formData, this.userID)
+      .subscribe(
+        res => this.uploadResponse = res,
+        err => this.error = err
+      )
   }
-
 }
