5f0a69331c7e4fdffaece91578752699b7b0cdcd
unknown <winoooops@outlook.com>
2019-08-28 18:17:28 +1000

    add mongo sever barebone to the project...

--- imgForumBackEnd/app.js
+++ imgForumBackEnd/app.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const forumRoute = require('./api/routes/forums')
+const db = require('./config/db')
 
 
 const app = express() 
 const PORT = 3000 
+
+
+
 
 app.use( express.static('public'))
 app.use( bodyParser.json() )
 app.use( bodyParser.urlencoded({ extended: false }))
 
 
-app.use('/', forumRoute )
+app.use('/', forumRoute)
 
 app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}`)
