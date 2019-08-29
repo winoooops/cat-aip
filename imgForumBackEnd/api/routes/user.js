648d0516a4110912f977081d416a70d888ef3524
unknown <winoooops@outlook.com>
2019-08-29 18:08:35 +1000

    bcrypt genearate salt and use it to hash the password..

--- imgForumBackEnd/api/routes/user.js
+++ imgForumBackEnd/api/routes/user.js
@@ -1,28 +1,42 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+
+const bcrypt = require('bcrypt')
+const saltRounds = 10 
+
 // check connection status with mongodb server
 
 /*****************************************
- *  provide login functionality
+ provide login functionality
+ 1. store the salt 
+ 2. hash the password 
+ 3. store the password hash, instead of passowrd
+ 4. store the record on MongoDB
 ******************************************/
-// show the login page <---- handle by angualr already
-
-router.get('/login', (req,res) => {
-    res.json(
-        { message: 'need a cup of tea'}
-    )
-})
 
 // post method
-router.post('/login', (req,res) => {
-    
-    User
-        .collection
-        .insertOne(req.body)
-        .then( (data) => {
-            res.json({"registed": true})
+router.post('/register', (req,res) => {
+    const { userId, email, pwd } = req.body
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+        if(err) {
+            console.log(err);
+            return
+        }
+        bcrypt.hash(pwd, salt, (err, hash) => {
+            if(err) {
+                console.log(err);
+                return
+            }
+            new User({
+                userId: userId, 
+                email: email, 
+                salt: salt,
+                hash: hash
+            }).save()        
         })
+        
+    }) 
 })
 
 
