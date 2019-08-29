ff5be664ea03c879393b762003d4b0313352ed66
unknown <winoooops@outlook.com>
2019-08-29 15:21:42 +1000

    email validator direactive with validor function

--- imgForum/src/app/user/shared/email.validator.ts
+++ imgForum/src/app/user/shared/email.validator.ts
@@ -3,7 +3,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms'
 const expression = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 export function EmailValidator(): ValidatorFn {
-    return ( control: AbstractControl ) :{ [key:string] : Boolean } => {
+    return ( control: AbstractControl ) :{ [key:string] : any } => {
         const valid = expression.test(control.value) 
         return valid ? null : {email: true}
     }
