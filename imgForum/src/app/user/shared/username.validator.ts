2568eb90f750365efa9fab484c1c8590a09b97be
weiwang <winoooops@outlook.com>
2019-09-02 13:26:56 +1000

    fix regular expression on username validator...

--- imgForum/src/app/user/shared/username.validator.ts
+++ imgForum/src/app/user/shared/username.validator.ts
@@ -2,11 +2,11 @@ import { AbstractControl, ValidatorFn } from '@angular/forms'
 
 // the regex below is from https://www.regextester.com/104030
 // the lenth of id should be between 3 to 16 characters 
-const expression = /^[a-z0-9_-]{3,16}$/ig
+const expression = /^[a-z0-9_-]{3,16}$/
 
 export function UsernameValidator(): ValidatorFn {
-    return( control: AbstractControl): { [key: string] : any} => {
-        const valid = expression.test( control.value )
+    return (control: AbstractControl): { [key: string]: any } => {
+        const valid = expression.test(control.value)
         return valid ? null : { username: true }
     }
 }
