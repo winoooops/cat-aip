e479ae5a6aa37fb392ee42473d3f2f0adbdea6c8
weiwang <winoooops@outlook.com>
2019-09-02 13:28:33 +1000

    add capital regular expression

--- imgForum/src/app/user/shared/username.validator.ts
+++ imgForum/src/app/user/shared/username.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms'
 
 // the regex below is from https://www.regextester.com/104030
 // the lenth of id should be between 3 to 16 characters 
-const expression = /^[a-z0-9_-]{3,16}$/
+const expression = /^[a-zA-Z0-9_-]{3,16}$/
 
 export function UsernameValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } => {
