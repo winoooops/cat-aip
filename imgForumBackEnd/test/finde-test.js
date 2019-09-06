917644902545e190a4b20e2537e79f519bdb6978
unknown <winoooops@outlook.com>
2019-09-06 17:23:32 +1000

    add some simple auto testing to the backend...

---
+++ imgForumBackEnd/test/finde-test.js
@@ -0,0 +1,42 @@
+const mocha = require('mocha')
+const assert = require('assert')
+const User = require('../api/models/user.js')
+
+
+// tells that this test is to test the finding functionality
+describe('finding test', () => {
+    let user 
+    // create and save a dummy data every time
+    beforeEach( (done) => {
+        user = new User({
+            userId: 'joe',
+            email: 'joe@uts.com',
+            salt: '', //salt and hash is for encryption, ignore them for now
+            hash: ''
+        })
+
+        user
+            .save()
+            .then( () => done() )
+    })
+})
+
+
+// offcially starts the testing
+it('Find document by userId from the database', (done) => {
+    User
+        .findOne({ userId: 'joe'})
+        .then( result => {
+            assert( result.userId === 'joe')
+            done()
+        })
+}) 
+
+it('find document by email from the database', (done) => {
+    User
+        .findOne({ email: 'joe@uts.com'})
+        .then( result => {
+            assert( result.email === 'joe@uts.com')
+            done()
+        })
+})
