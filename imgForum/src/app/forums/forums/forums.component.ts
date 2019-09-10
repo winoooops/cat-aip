eb9b93822f80c461fdfd4b886b41783588e3fd7f
unknown <winoooops@outlook.com>
2019-09-10 16:16:00 +1000

    functionality of reading the database image and display them on the forums route(but cors issues encountered...)

--- imgForum/src/app/forums/forums/forums.component.ts
+++ imgForum/src/app/forums/forums/forums.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ImageService, Image } from '../services/image.service';
 
 @Component({
-  selector: 'app-forums',
+  selector: 'app-forum',
   templateUrl: './forums.component.html',
   styleUrls: ['./forums.component.scss']
 })
 export class ForumsComponent implements OnInit {
+  images: Image[]
 
-  constructor() { }
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
+    this.imageService.getImageData()
+      .subscribe( (images) => {
+        this.images = images
+        console.log( this.images )
+      })
   }
 
 }
