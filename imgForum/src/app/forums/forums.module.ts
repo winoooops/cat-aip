b01b152a3417970e258a017daa7abd62da5f21ba
unknown <winoooops@outlook.com>
2019-09-10 14:51:57 +1000

    continue to last commmit

--- imgForum/src/app/forums/forums.module.ts
+++ imgForum/src/app/forums/forums.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
+// import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common';
 import { ForumsComponent } from './forums/forums.component';
 import { Routes, RouterModule } from '@angular/router'
 import { ForumComponent } from './forum/forum.component';
-import { ForumPostComponent } from './forum-post/forum-post.component';
+// import { ForumPostComponent } from './forum-post/forum-post.component';
 import { MaterialUiModule } from '../material-ui/material-ui.module';
 import { ThreadsComponent } from './threads/threads.component';
 import { ThreadComponent } from './thread/thread.component';
+import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
-  { path: 'post', component: ForumPostComponent },
+  { path: 'post', component: PostComponent },
   { path: ':forum_alias', component: ForumComponent },
   { path: '', component: ForumsComponent },
 ]
@@ -19,11 +22,14 @@ const routes: Routes = [
   declarations: [
     ForumsComponent,
     ForumComponent,
-    ForumPostComponent,
+    // ForumPostComponent,
     ThreadsComponent,
-    ThreadComponent
+    ThreadComponent,
+    PostComponent,
   ],
   imports: [
+    CommonModule,
+    FormsModule,
     RouterModule.forChild(routes),
     MaterialUiModule
   ],
