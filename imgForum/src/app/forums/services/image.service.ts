050ee23103e50983302d9abd5e3d33ff2c0ca216
unknown <winoooops@outlook.com>
2019-09-10 16:16:00 +1000

    functionality of reading the database image and display them on the forums route(but cors issues encountered...)

--- imgForum/src/app/forums/services/image.service.ts
+++ imgForum/src/app/forums/services/image.service.ts
@@ -6,7 +6,8 @@ const SERVER_URL = "http://localhost:3000"
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    // "Access-Control-Allow-Origin": "*"
   })
 };
 
@@ -28,5 +29,9 @@ export class ImageService {
 
   saveImageData(data): Observable<Image> {
     return this.http.post<any>(`${SERVER_URL}/forums/post`, data, httpOptions)
+  }
+
+  getImageData(): Observable<Array<Image>> {
+    return this.http.get<any>(`${SERVER_URL}/forums/`, httpOptions)
   }
 }
