12666f4fe4d2eff6fea94e3b81f57a741ee88e99
unknown <winoooops@outlook.com>
2019-09-16 22:10:07 +1000

    apply multer to enable file/image upload functionality...

--- imgForumBackEnd/api/routes/forums.js
+++ imgForumBackEnd/api/routes/forums.js
@@ -1,27 +1,59 @@
 const express = require('express')
 const router = express.Router() 
 const Image = require('../models/image')
-// router.get('/post', (req,res) => {
-//     res.send('showing the uplaod form ')
-// })
+const multer = require('multer')
 
-router.post('/post', (req,res) => {
-   
-    // allow user to upload the image's url 
+
+ // define a storage location and naming strategy <---- multer enabled 
+ const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req,file,cb) => {
+        cb(null, file.filename + '-' + Date.now() )
+    } 
+})
+
+const upload = multer({ storage: storage })
+
+
+
+
+router.post('/post', upload.single('myPic'), (req,res) => {
+    // allow user to upload the image's url -> users are now required to upload img from disk (updated 16/09/19)
     // automatically takes down user's id
     // created an empty array for comments 
 
     // save records to the database
     // redirect and show uploaded image
-    const { url, author } = req.body
-    new Image({ url, author})
-            .save()
-            .then( () => {
-                res.json({
-                    "message": "image uploaded..." 
-                 })
+
+    // parts of the code below is inspired from `https://code.tutsplus.com/tutorials/file-upload-with-multer-in-node--cms-32088`
+    const file = req.file
+    if( !file ) {
+        return 
+    }
+
+    const img = fs.readFileSync( req.file.path )
+    const encode_img = img.toString('base64') // encode the img to a base64 string 
+
+    new Image({
+        data: new Buffer(encode_img, 'base64'),
+        contentType: req.file.mimetype, 
+        author: req.body.author
+    })
+        .save()
+        .then( () => {
+            res.json({
+                "message": "image uploaded..."
             })
+        })
 })
+
+
+
+
+
+
 
 router.get('/', (req,res) => {
     // read the image data
