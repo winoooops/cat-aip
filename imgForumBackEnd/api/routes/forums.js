767840842f765d36786501eef8e1d034c712f971
unknown <winoooops@outlook.com>
2019-09-16 23:53:02 +1000

    use multer to store the img file locally in backend...

--- imgForumBackEnd/api/routes/forums.js
+++ imgForumBackEnd/api/routes/forums.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
 const router = express.Router() 
 const Image = require('../models/image')
 const multer = require('multer')
 
 
+
  // define a storage location and naming strategy <---- multer enabled 
  const storage = multer.diskStorage({
     destination: (req,file,cb) => {
-        cb(null, 'uploads')
+        cb(null, path.join(__dirname + '../../../images') )
     },
     filename: (req,file,cb) => {
         cb(null, file.filename + '-' + Date.now() )
@@ -19,7 +22,7 @@ const upload = multer({ storage: storage })
 
 
 
-router.post('/post', upload.single('myPic'), (req,res) => {
+router.post('/post', upload.single('image'), (req,res) => {
     // allow user to upload the image's url -> users are now required to upload img from disk (updated 16/09/19)
     // automatically takes down user's id
     // created an empty array for comments 
@@ -28,18 +31,22 @@ router.post('/post', upload.single('myPic'), (req,res) => {
     // redirect and show uploaded image
 
     // parts of the code below is inspired from `https://code.tutsplus.com/tutorials/file-upload-with-multer-in-node--cms-32088`
+    
     const file = req.file
     if( !file ) {
         return 
     }
 
+    console.log( file )
+
     const img = fs.readFileSync( req.file.path )
     const encode_img = img.toString('base64') // encode the img to a base64 string 
 
     new Image({
-        data: new Buffer(encode_img, 'base64'),
-        contentType: req.file.mimetype, 
-        author: req.body.author
+        img: {
+            data: new Buffer(encode_img, 'base64'),
+            contentType: req.file.mimetype, 
+        }
     })
         .save()
         .then( () => {
