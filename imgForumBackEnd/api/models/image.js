a33e4dec142de412698d6240e06f0c663bff89fa
unknown <winoooops@outlook.com>
2019-09-16 21:41:42 +1000

    change the schema and model of image in mongodb...

--- imgForumBackEnd/api/models/image.js
+++ imgForumBackEnd/api/models/image.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const imgSchema = new Schema({
-    url: String,
-    author: String,
-    // comments: Array
+    img: {
+        data: Buffer, 
+        contentType: String
+    }, 
+    timestamps: true // auto save and update
 })
 
 module.exports = mongoose.model('Image', imgSchema)
