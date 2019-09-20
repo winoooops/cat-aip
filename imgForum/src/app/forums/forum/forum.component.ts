2f4b26308f88f22c24ad8ce66ee1251adc1b7d63
unknown <winoooops@outlook.com>
2019-09-20 15:52:36 +1000

    further developed forum routing

--- imgForum/src/app/forums/forum/forum.component.ts
+++ imgForum/src/app/forums/forum/forum.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../services/image.service';
+import { Forum } from 'src/app/services/data';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-forum',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forum.component.scss']
 })
 export class ForumComponent implements OnInit {
-
-  constructor() { }
+  forum: Forum
+  constructor(
+    private imageService: ImageService,
+    private route: ActivatedRoute  
+  ) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe( (params: Params) => {
+      this.forum = this.imageService.forum( params['alias'])
+    })
   }
 
 }
