970732db334a06ab1b3d4dad8f17eafd2ef73839
weiwang <winoooops@outlook.com>
2019-10-03 16:51:25 +1000

    git can't seem to automatically fix the conflices, so had to manually change the login.component.ts...

--- imgForum/src/app/login/login.component.ts
+++ imgForum/src/app/login/login.component.ts
@@ -1,67 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { first } from 'rxjs/operators';
-import { AlertService, AuthenticationService } from '@/services';
-
-import { from } from 'rxjs';
+import { Component } from '@angular/core';
+import { UserService } from '../user/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  // connecting to the backend stuff using fetch api
+  username: string
+  password: string
 
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
+
   constructor(
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private authenticationService: AuthenticationService,
-    private alertService: AlertService
-  ) {
-    if(this.authenticationService.currentUserValue) {
-      this.router.navigate(['/']);
-    }
-   }
+    private userService: UserService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl']||'/';
+  onSubmit() {
+    this.userService.login({
+      userId: this.username,
+      password: this.password
+    }).subscribe((res) => {
+      alert(res.id);
+      this.router.navigate(['forums/post'], { queryParams: { id: res.id } })
+    })
   }
-
-  get f() {return this.loginForm.controls;}
-
-  onSubmit(){
-    this.submitted = true;
-
-    this.alertService.clear();
-
-    if(this.loginForm.invalid) {
-      return;
-    }
-
-    this.loading = true;
-    this.authenticationService.login(this.f.username.value, this.f.password.value)
-    .pipe(first())
-    .subscribe(
-      data => {
-        this.router.navigate([this.returnUrl]);
-      },
-      error => {
-        this.alertService.error(error);
-        this.loading = false;
-      }
-      );
-
-  }
-
 }
