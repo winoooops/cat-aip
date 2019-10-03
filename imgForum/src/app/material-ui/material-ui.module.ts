820e63f9c80bac4b18a5725a9a0ac8a919aa26ff
unknown <winoooops@outlook.com>
2019-10-03 21:28:51 +1000

    complement to last commits...

---
+++ imgForum/src/app/material-ui/material-ui.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatCardModule } from '@angular/material/card'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatInputModule } from '@angular/material/input'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatIconModule } from '@angular/material/icon'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatChipsModule } from '@angular/material/chips'
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ], 
+  exports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatInputModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatListModule,
+    MatChipsModule
+  ]
+})
+export class MaterialUiModule { }
