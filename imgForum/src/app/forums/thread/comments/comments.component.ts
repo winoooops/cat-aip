f935460463ab8f2ce70229bdd1fa26c910692462
unknown <winoooops@outlook.com>
2019-10-05 16:29:15 +1000

    just like threads, comments component are now managing all the comment components to which it passed down the query ids...

---
+++ imgForum/src/app/forums/thread/comments/comments.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ImageService } from '../../services/image.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+  @Input() id: string
+  idArr: string[]
+  constructor(
+    private imageService: ImageService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.imageService
+      .getCommentImages(this.id)
+      .subscribe( r => {
+        this.idArr = r.map( doc => doc._id)
+        console.log( this.idArr )
+      })
+  }
+
+  addComment() {
+    console.log( this.id )
+    this.router.navigate(['/forums/post'], {
+      queryParams: {
+        commentOn: this.id
+      }
+    })
+  }
+
+
+}
