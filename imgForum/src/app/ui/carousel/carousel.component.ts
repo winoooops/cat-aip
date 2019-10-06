c160db61df65be1730136225ce96c232dd144488
unknown <winoooops@outlook.com>
2019-10-07 08:13:42 +1100

    reuse imageService's method to get an array containing the base64 img src converted from img info from the database...

--- imgForum/src/app/ui/carousel/carousel.component.ts
+++ imgForum/src/app/ui/carousel/carousel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from 'src/app/forums/services/image.service';
+import { arrayBufferToBase64 } from '../../forums/shared/convertB64'
 
 @Component({
   selector: 'app-carousel',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-
-  constructor() { }
+  imgSrcArr: string[] = []
+  constructor( private imageService: ImageService) { }
 
   ngOnInit() {
+    this.imageService
+      .getHotThreads()
+      .subscribe( docArr => {
+        // console.log( docArr )
+        docArr.forEach( doc => {
+          this.imageService
+            .getDocData( doc._id )
+            .subscribe( doc => {
+              const flag = `data:${doc.img.contentType};base64,`
+              const imgStr = arrayBufferToBase64(doc.img.data.data)
+              // console.log( imgStr )
+              // this.author = doc.author
+              this.imgSrcArr.push(flag + imgStr)
+              console.log( this.imgSrcArr )
+            })
+        })
+      })
   }
 
 }
