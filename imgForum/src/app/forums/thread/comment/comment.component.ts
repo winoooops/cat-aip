9935bc998e176c74aa560aa179ae1c8dcef09c08
unknown <winoooops@outlook.com>
2019-10-06 20:03:14 +1100

    so deos the comment page...

--- imgForum/src/app/forums/thread/comment/comment.component.ts
+++ imgForum/src/app/forums/thread/comment/comment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ImageService } from '../../services/image.service';
 import { arrayBufferToBase64 } from '../../shared/convertB64';
+import { EmojiDialogComponent } from '../emoji-dialog/emoji-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-comment',
@@ -17,7 +19,10 @@ export class CommentComponent implements OnInit {
   time: string
   counts: number
   isCommentsViewable: boolean = false
-  constructor(private imageService: ImageService) { }
+  constructor(
+    private imageService: ImageService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
     this.imageService
@@ -40,4 +45,7 @@ export class CommentComponent implements OnInit {
     this.isCommentsViewable = !this.isCommentsViewable
   }
 
+  openEmojiDialog() {
+    this.dialog.open(EmojiDialogComponent)
+  }
 }
