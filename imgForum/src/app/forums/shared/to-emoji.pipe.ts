df1dc297550577e279e13f620c4945f74cb5e4c3
unknown <winoooops@outlook.com>
2019-10-07 08:54:33 +1100

    fix issue of showing: RangeError: Invalid code point for sometimes emoji is undefined in comment component...

--- imgForum/src/app/forums/shared/to-emoji.pipe.ts
+++ imgForum/src/app/forums/shared/to-emoji.pipe.ts
@@ -7,9 +7,12 @@ export class ToEmojiPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
     // the idea of transfering a string to emoji was inspired by https://medium.com/@allegra9/add-emoji-picker-to-your-react-chat-app-30d8cbe8d9a6
-    const codesArray = []
-    const point = '0x'+ value 
-    codesArray.push( point )
-    return String.fromCodePoint(...codesArray )
+    if( value ) {
+      const codesArray = []
+      const point = '0x'+ value 
+      codesArray.push( point )
+      return String.fromCodePoint(...codesArray )
+    }
+    
 }
 }
