ff3c325fdd8a37d844f6eacacfab6f903659e3fc
unknown <winoooops@outlook.com>
2019-10-08 02:51:54 +1100

    add ways to easily tell if the content could be changed...

--- imgForum/src/app/forums/thread/comment/comment.component.ts
+++ imgForum/src/app/forums/thread/comment/comment.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ImageService } from '../../services/image.service';
 import { arrayBufferToBase64 } from '../../shared/convertB64';
 import { EmojiDialogComponent } from '../emoji-dialog/emoji-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { relativeTimeThreshold } from 'moment';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { UserService } from 'src/app/user/services/user.service';
+
 
 @Component({
   selector: 'app-comment',
@@ -21,16 +24,29 @@ export class CommentComponent implements OnInit {
   time: string
   counts: number
   emoji: string 
+  url: string
   isCommentsViewable: boolean = false
+  isMutable: boolean = false 
   constructor(
     private imageService: ImageService,
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
     public dialog: MatDialog
   ) { }
   
   ngOnInit() {
+    // const forums_ali = this.route.snapshot.parent.params['forum_alias']
+    // const thread_ali = this.route.snapshot.params['thread_alias']
+    // this.url = `/forums/${forums_ali}/${thread_ali}`
+    // console.log( this.url )
+    
     this.imageService
       .getDocData(this.id)
       .subscribe(doc => {
+        if( doc.author === localStorage.getItem('username')) {
+          this.isMutable = true;
+        }
         if( doc.img ) {
           this.isImage = true 
           // get the contentType 
@@ -55,8 +71,12 @@ export class CommentComponent implements OnInit {
   }
 
   openEmojiDialog() {
-    this.dialog.open(EmojiDialogComponent, {
-      data: { "commentOn": this.id } 
+    let dialogRef = this.dialog.open(EmojiDialogComponent, {
+      data: { "commentOn": this.id, } 
     })
+
+    dialogRef.afterClosed().subscribe(result => {
+      // should refresh the parent 
+    });
   }
 }
