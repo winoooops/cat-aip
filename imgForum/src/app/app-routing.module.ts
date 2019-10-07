7b7b2c38592ca4e3fd0a19e2a351457ef9be7d8e
unknown <winoooops@outlook.com>
2019-10-08 02:37:19 +1100

    change the default route for the application

--- imgForum/src/app/app-routing.module.ts
+++ imgForum/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'forums', loadChildren: 'src/app/forums/forums.module#ForumsModule' },
   { path: 'user', loadChildren: 'src/app/user/user.module#UserModule'},
-  { path: '', redirectTo: '/forums', pathMatch: 'full' }
+  { path: '', redirectTo: '/forums/all', pathMatch: 'full' }
 ];
 
 @NgModule({
