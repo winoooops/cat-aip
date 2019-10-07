70a87716136796bc0d53f4fa655d64880db40a39
unknown <winoooops@outlook.com>
2019-10-08 02:00:54 +1100

    add commnet in auth interceptor

--- imgForum/src/app/user/services/auth.interceptor.ts
+++ imgForum/src/app/user/services/auth.interceptor.ts
@@ -9,10 +9,11 @@ export class AuthInterceptor implements HttpInterceptor {
     // the code snippet is from https://blog.angular-university.io/angular-jwt-authentication/
     intercept(req: HttpRequest<any>,
               next: HttpHandler): Observable<HttpEvent<any>> {
-
+        // take the token from localStorage
         const idToken = localStorage.getItem("id_token");
 
         if (idToken) {
+            // if it exists, add an Authenrization header with jwt to the request
             const cloned = req.clone({
                 headers: req.headers.set("Authorization", idToken)
             });
